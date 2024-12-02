9cc72b0e9136123440e0f2f65a6758772839f677
choisunmi00 <keunangyung01@naver.com>
2024-12-02 22:36:22 +0900

    modal

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -11,16 +11,25 @@
 //   }
   
 
-$(function(){
-    // 이미지 클릭시 해당 이미지 모달
-    $("span img").click(function(){
-        let img = new Image();
-        img.src = $(this).attr("src")
-        $('.modalBox').html(img);
-        $(".modal").show();
+$(function () {
+    // 이미지 클릭 시 모달 표시
+    $(".author__avatar img").click(function () {
+      let img = new Image();
+      img.src = $(this).attr("src");
+      img.style.maxWidth = "100%";
+      img.style.maxHeight = "100%";
+      $('.modalBox').html(img);
+      $(".modal").fadeIn(); // 모달 표시
     });
-    // 모달 클릭할때 이미지 닫음
-    $(".modal").click(function (e) {
-    	$(".modal").toggle();
+  
+    // 모달 클릭 시 닫기
+    $(".modal").click(function () {
+      $(this).fadeOut(); // 모달 닫기
     });
-});
+  
+    // 모달 내부 이미지 클릭 시 닫히지 않도록 이벤트 전파 방지
+    $(".modalBox").click(function (e) {
+      e.stopPropagation();
+    });
+  });
+  
